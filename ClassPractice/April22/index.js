4dce566a0cad0db76240e8335ac632fe922992cd
kavithashanmugan <kavitha.shanmugan@gmail.com>
2021-04-25 23:16:10 -0600

    saving changes

---
+++ ClassPractice/April22/index.js
@@ -0,0 +1,29 @@
+console.log(2+2);
+console.log("I like javascript")
+
+var myName;
+var age = 20;
+var y=5;
+let x =10;
+const pi = 3.14;
+var rollNumber,address;
+
+myName = "Kavitha";
+
+var firstName = "Kavitha"
+var lastName = "Shanmugan"
+console.log("My name is" + firstName + " " + lastName)
+console.log("My name is:",myName);
+var finalString = `Welcome ${myName} ${lastName}`;
+console.log("back tick method",finalString)
+
+function add(){
+    y = 10;
+    let x = 12;
+   // pi=4;
+    console.log(x+pi,"x")
+    console.log("y",y)
+}
+
+add();
+console.log(y)
---
+++ js/index1.js
@@ -0,0 +1,9 @@
+var firstName = "Kavitha";
+var lastName = "Shanmugan";
+var age =27;
+var address = "202,431 3rd Ave N";
+var dateOfBirth = "01/11/1993";
+
+console.log("firstName",firstName);
+console.log("")
+
